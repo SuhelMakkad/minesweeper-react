6ca34ddaf908d3c5b761f63142fabed211606b98
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-05-09 21:17:16 +0530

    added alises

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import path from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/minesweeper-react/",
+  resolve: {
+    alias: {
+      "@/components": path.resolve(__dirname, "./src/common/components"),
+      "@/hooks": path.resolve(__dirname, "./src/common/hooks"),
+      "@/images": path.resolve(__dirname, "./src/assets/images"),
+      "@/assets": path.resolve(__dirname, "./src/assets"),
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react()],
 });
